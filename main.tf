bbd6089397971fd95e2127645fab2954a83c6e06
Jared Groves <jgroves27@gmail.com>
2026-06-26 10:41:52 -0400

    fix(ci): Federated is single sting, not array.

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = [data.aws_iam_openid_connect_provider.github.arn]
+          Federated = data.aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
